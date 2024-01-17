bad147d672fe0c73e2d7e0ed204a80a9f29a838c
joonamin <joonamin44@gmail.com>
2024-01-17 21:46:49 +0900

    config: add `githook` configuration

---
+++ backend/Farmily/src/main/java/com/ssafy/farmily/FarmilyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.farmily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmilyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmilyApplication.class, args);
+    }
+
+}
