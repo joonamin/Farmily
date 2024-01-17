24f6cac2bc9f2cbbad31a98b81fa16ca8965753e
joonamin <joonamin44@gmail.com>
2024-01-18 02:35:23 +0900

    feat&fix: checkstyle 적용 및 pre-commit hook 수정

---
+++ backend/Farmily/src/test/java/com/ssafy/farmily/FarmilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.farmily;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmilyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
