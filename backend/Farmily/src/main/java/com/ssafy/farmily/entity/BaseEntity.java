10cca3be54e6ae9d0dd7061a98ae09b3faa6181d
구본웅 <thesmartist3@gmail.com>
2024-01-22 12:28:18 +0900

    fix: JPA Auditing(자동 날짜 삽입) 활성화

--- backend/Farmily/src/main/java/com/ssafy/farmily/entity/BaseEntity.java
+++ backend/Farmily/src/main/java/com/ssafy/farmily/entity/BaseEntity.java
@@ -4,8 +4,10 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -18,6 +20,7 @@ import lombok.experimental.SuperBuilder;
  * @author	구본웅
  */
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @SuperBuilder
 @RequiredArgsConstructor
 public abstract class BaseEntity {
