3c4beccb38f7a5841595d2cc7f172970c1241fb3
지민 <81950388+jjm6604@users.noreply.github.com>
2024-01-22 11:51:13 +0900

    feat: sidebar 추가

--- frontend/farmily/src/App.jsx
+++ frontend/farmily/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import MainPage from './pages/MainPage'
 
-export default function App() {
 
-  return (
-    <>
-      <MainPage />
-    </>
-  )
+const router = createBrowserRouter([
+  { path: '/', element: <MainPage />},
+
+]);
+export default function App() {
+  return <RouterProvider router={router} />;
 }
