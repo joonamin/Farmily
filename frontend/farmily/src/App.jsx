18f5851b8df0a82214396f7f160ad2096091cbdf
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-01-22 11:20:00 +0900

    feat: 메인페이지 이미지 구성

---
+++ frontend/farmily/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+
+import MainPage from './pages/MainPage'
+
+export default function App() {
+
+  return (
+    <>
+      <MainPage />
+    </>
+  )
+}
---
+++ frontend/farmily/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    backgroundImage: {
+      main: "url('./assets/images/background.png')",
+    },
+  },
+  plugins: [],
+};
