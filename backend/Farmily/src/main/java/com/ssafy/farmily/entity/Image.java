008be045e40fbcca9658d94a6906ef322633fc61
구본웅 <thesmartist3@gmail.com>
2024-01-23 09:59:48 +0900

    fix: conflict

--- backend/Farmily/src/main/java/com/ssafy/farmily/entity/Image.java
+++ backend/Farmily/src/main/java/com/ssafy/farmily/entity/Image.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
