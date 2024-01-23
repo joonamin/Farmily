bbba89251a47ca2313f8b93650b581b3576c5b28
지민 <81950388+jjm6604@users.noreply.github.com>
2024-01-23 14:16:33 +0900

    feat: 기록하기(조회) 페이지

---
+++ frontend/farmily/src/components/ArticleItem.jsx
@@ -0,0 +1,10 @@
+export default function ArticleItem(article){
+  return (
+    // 게시글 디테일 페이지 만든 후 Link 달아주기
+    <tr className="border-b-2">
+      <td>{ article.title }</td>
+      <td>{ article.author }</td>
+      <td>{ article.date }</td>
+    </tr>
+  )
+}
--- frontend/farmily/src/pages/RecordPage.jsx
+++ frontend/farmily/src/pages/RecordPage.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react'
-
+import ArticleList from '../components/ArticleList.jsx'
+import ImageList from '../components/ImageList.jsx'
 
 export default function RecordPage() {
   return (
     <>
       <h1>기록 페이지</h1>
+      <ImageList />
+      <ArticleList />
     </>
   )
 }
