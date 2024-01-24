05fe257ae54a866567b090c067df1ea33ff780b9
지민 <81950388+jjm6604@users.noreply.github.com>
2024-01-24 12:35:19 +0900

    feat: 업적 페이지 구성

--- frontend/farmily/src/pages/AchievementPage.jsx
+++ frontend/farmily/src/pages/AchievementPage.jsx
@@ -1,10 +1,44 @@
 import { useState, useEffect } from 'react'
+import AchievementList from '../components/AchievementList.jsx'
+
+
+// 테스트 data // 데이터 받아오면 삭제
+const TESTITEMS = [
+  { content: '도전과제 101개 하기', percent: 90, reward: false },
+  { content: '도전과제 102개 하기', percent: 0, reward: false },
+  { content: '도전과제 102개 하기', percent: 0, reward: false },
+  { content: '도전과제 14개 하기', percent: 0, reward: false },
+  { content: '도전과제 105개 하기', percent: 0, reward: false },
+  { content: '도전과제 110개 하기', percent: 100, reward: false },
+  { content: '도전과제 16개 하기', percent: 100, reward: true },
+  { content: '도전과제 16개 하기', percent: 100, reward: true },
+  { content: '도전과제 110개 하기', percent: 100, reward: false },
+  { content: '도전과제 110개 하기', percent: 100, reward: false },
+  { content: '도전과제 106개 하기', percent: 22, reward: false },
+  { content: '도전과제 106개 하기', percent: 70, reward: false },
+  { content: '도전과제 1012개 하기', percent: 50, reward: false },
+  { content: '도전과제 140개 하기', percent: 40, reward: false },
+  { content: '도전과제 130개 하기', percent: 30, reward: false },
+
+]
+
 
 export default function AchievementPage() {
-
+  const beforeTasks = filteredByPercent(0, 0);
+  const ongoingTasks = filteredByPercent(1, 99);
+  const finishedTasks = filteredByPercent(100, 100);
+  
   return (
     <>
-      <h1>업적 페이지</h1>
+      <AchievementList title='시작 전 업적' tasks={beforeTasks} />
+      <AchievementList title='진행 중 업적' tasks={ongoingTasks} />
+      <AchievementList title='완료 업적' tasks={finishedTasks} />
     </>
   )
 }
+
+const filteredByPercent = (minPercent, maxPercent) => {
+  return TESTITEMS.filter(
+    (item) => item.percent >= minPercent && item.percent <= maxPercent
+  );
+};
--- frontend/farmily/src/pages/CommunityPage.jsx
+++ frontend/farmily/src/pages/CommunityPage.jsx
@@ -5,7 +5,6 @@ import CommunityItem from '../components/CommunityItem.jsx';
 const TESTITEMS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 export default function CommunityPage() {
-
   return (
     <div className="h-full overflow-hidden p-5">
       <h1>커뮤니티 페이지</h1>
