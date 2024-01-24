8ebe8bf8dbecdd232d9c90b82665cf232724c4c6
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-01-24 23:38:56 +0900

    feat: 캘린더 수정중

--- frontend/farmily/src/pages/CalendarPage.jsx
+++ frontend/farmily/src/pages/CalendarPage.jsx
@@ -3,7 +3,7 @@ import MyCalendar from '../components/FullCalendar.jsx';
 
 export default function CalendarPage() {
   return (
-    <div className="">
+    <div className="h-full">
       <MyCalendar />
     </div>
   );
