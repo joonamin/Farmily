704422f71ef58e091903395f11a677197b97413e
구본웅 <thesmartist3@gmail.com>
2024-01-29 12:40:13 +0900

    feat: PlacementDto validate

--- backend/Farmily/src/main/java/com/ssafy/farmily/dto/PlacementDto.java
+++ backend/Farmily/src/main/java/com/ssafy/farmily/dto/PlacementDto.java
@@ -1,6 +1,9 @@
 package com.ssafy.farmily.dto;
 
 import com.ssafy.farmily.type.AccessoryType;
+import com.ssafy.farmily.validation.annotation.InRectangle;
+
+import jakarta.validation.constraints.NotNull;
 import utils.Position;
 
 import lombok.Data;
@@ -9,20 +12,12 @@ import lombok.ToString;
 @Data
 @ToString
 public class PlacementDto {
+	@InRectangle(minCol = 0, maxCol = 99, minRow = 0, maxRow = 99)
 	private Position position;
+
 	private String dtype;
+
 	private Long recordId;
+
 	private AccessoryType type;
-	// 여기로 오면은요 이렇게 있는데용
-	// 여기가 속살인가요? 존나 꼴궁속입니다요네리
-	// 그래서 저는요
-	// {
-	//         "dtype":"F",
-	//         "position":{
-	//             "row":3,
-	//             "col":3
-	//         },
-	//         "recordId":4
-	//         }
-	// placement를 이렇게 주면 expect 되어 있는 FruitPlacement로
 }
