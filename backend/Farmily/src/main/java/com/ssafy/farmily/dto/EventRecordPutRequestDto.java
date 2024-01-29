69b4c629ed733dc7107f51d1dde3ac097631b51f
구본웅 <thesmartist3@gmail.com>
2024-01-29 12:10:55 +0900

    feat: validate Record Post/Put, Placing Item

--- backend/Farmily/src/main/java/com/ssafy/farmily/dto/EventRecordPutRequestDto.java
+++ backend/Farmily/src/main/java/com/ssafy/farmily/dto/EventRecordPutRequestDto.java
@@ -3,6 +3,8 @@ package com.ssafy.farmily.dto;
 import java.util.List;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,7 +18,11 @@ import lombok.Setter;
 @Builder
 @Schema(description = "이벤트기록 수정 요청 DTO")
 public class EventRecordPutRequestDto {
+	@NotNull
 	private Long recordId;
+
+	@NotBlank
 	private String title;
+
 	private List<ImageCardRequestDto> imageCards;
 }
--- backend/Farmily/src/main/java/com/ssafy/farmily/dto/ImageCardRequestDto.java
+++ backend/Farmily/src/main/java/com/ssafy/farmily/dto/ImageCardRequestDto.java
@@ -3,6 +3,7 @@ package com.ssafy.farmily.dto;
 import org.springframework.web.multipart.MultipartFile;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,6 +17,8 @@ import lombok.Setter;
 @Builder
 @Schema(description = "요청 시 이미지 카드 DTO")
 public class ImageCardRequestDto {
+	@NotNull
 	private MultipartFile imageFile;
+
 	private String description;
 }
