b8b0f6c8fd05942a1c92e04e029b876198fe6749
지민 <81950388+jjm6604@users.noreply.github.com>
2024-01-29 12:16:40 +0900

    feat: 기록(일상, 챌린지) 글 작성

--- frontend/farmily/src/pages/DailyCreatePage.jsx
+++ frontend/farmily/src/pages/DailyCreatePage.jsx
@@ -1,12 +1,50 @@
 import { useState, useEffect } from 'react';
-import CreateDetail from '../components/common/CreateDetail.jsx';
+import { useNavigate } from 'react-router-dom';
+import CreateDetail from '../components/common/CreateDetail';
 import SmallButton from '../components/button/SmallButton.jsx';
+import axios from '../api/axios.jsx';
 
 export default function DailyCreatePage() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    // 로그인 후 sprintId 받아오면 수정하기
+    sprintId: 1,
+    title: '',
+    content: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleClick = () => {
+    // 로그인 후 유저 정보 같이 보내기
+    axios
+      .post('/record/daily', formData)
+      .then((response) => {
+        navigate('/family/record');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
-      <h1>일상 글쓰기</h1>
-      <CreateDetail />
+      <div className="h-5/6">
+        <h1>일상 글쓰기</h1>
+        <CreateDetail
+          title={formData.title}
+          content={formData.content}
+          onInputChange={handleChange}
+        />
+      </div>
+      <div className="h-1/6">
+        <span onClick={handleClick}>
+          <SmallButton text="글쓰기" />
+        </span>
+      </div>
     </>
   );
 }
