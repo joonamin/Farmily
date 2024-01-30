48883084f4ccb368540f969eaf0dd48886287930
지민 <81950388+jjm6604@users.noreply.github.com>
2024-01-30 10:51:32 +0900

    feat: 일상 상세 페이지

--- frontend/farmily/src/components/common/ArticleDetail.jsx
+++ frontend/farmily/src/components/common/ArticleDetail.jsx
@@ -7,7 +7,12 @@ export default function ArticleDetail(props) {
         <div className="border-4 p-2 border-black rounded-xl text-left w-full">
           <strong>{props.title}</strong>
         </div>
-        <p className="text-right px-10">작성자 : 작성자</p>
+        <div className="flex justify-between">
+          <span className="text-left">
+            작성일자 : {props.createdAt.slice(0, 10)}
+          </span>
+          <span className="text-right">작성자 : {props.author.nickname}</span>
+        </div>
         <div className="border-4 p-2 border-black rounded-xl text-left w-full h-60">
           <p>{props.content}</p>
         </div>
--- frontend/farmily/src/pages/DailyDetailPage.jsx
+++ frontend/farmily/src/pages/DailyDetailPage.jsx
@@ -6,22 +6,25 @@ import axios from '../api/axios.jsx';
 
 export default function DailyDetailPage() {
   const [record, setRecord] = useState({
-    title: '제목',
-    content: '내용',
+    title: '',
+    content: '',
+    createdAt: '',
+    author: { nickname: '' },
   });
-  axios
-    .get('/record/1')
-    .then((response) => {
-      console.log(response.data);
-      setRecord(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  useEffect(() => {
+    axios
+      .get('/record/1')
+      .then((response) => {
+        setRecord(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="overflow-y-auto max-h-full p-10">
-      <ArticleDetail title={record.title} content={record.content} />
+      <ArticleDetail {...record} />
       <Comment />
     </div>
   );
