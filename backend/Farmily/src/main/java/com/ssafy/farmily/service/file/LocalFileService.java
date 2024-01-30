13d09fd67f70ef53bfe3aeb2d2b734b007768c59
구본웅 <thesmartist3@gmail.com>
2024-01-30 11:46:35 +0900

    refactor: @SneakyThrows 제거

--- backend/Farmily/src/main/java/com/ssafy/farmily/service/file/LocalFileService.java
+++ backend/Farmily/src/main/java/com/ssafy/farmily/service/file/LocalFileService.java
@@ -13,8 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 import com.ssafy.farmily.dto.ImageDto;
 import com.ssafy.farmily.repository.ImageRepository;
 
-import lombok.SneakyThrows;
-
 @Service
 public class LocalFileService extends AbstractFileService {
 
@@ -26,20 +24,24 @@ public class LocalFileService extends AbstractFileService {
 	}
 
 	@Override
-	@SneakyThrows
 	public ImageDto uploadImage(MultipartFile file) {
-		File directory = new File(path);
-		if (!directory.isDirectory()) {
-			directory.mkdirs();
+		try {
+			File directory = new File(path);
+			if (!directory.isDirectory()) {
+				directory.mkdirs();
+			}
+			String originalFileName = file.getOriginalFilename();
+			String newFileName = UUID.randomUUID().toString();
+			String extension = StringUtils.getFilenameExtension(originalFileName);
+			String newPath = path + File.separator + newFileName + "." + extension;
+
+			File newFile = new File(newPath);
+			file.transferTo(newFile);
+
+			return ImageDto.of(newPath, originalFileName);
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
 		}
-		String originalFileName = file.getOriginalFilename();
-		String newFileName = UUID.randomUUID().toString();
-		String extension = StringUtils.getFilenameExtension(originalFileName);
-		String newPath = path + File.separator + newFileName + "." + extension;
 
-		File newFile = new File(newPath);
-		file.transferTo(newFile);
-
-		return ImageDto.of(newPath, originalFileName);
 	}
 }
--- backend/Farmily/src/main/java/com/ssafy/farmily/service/webrtc/OpenViduWebRtcService.java
+++ backend/Farmily/src/main/java/com/ssafy/farmily/service/webrtc/OpenViduWebRtcService.java
@@ -9,6 +9,7 @@ import com.ssafy.farmily.exception.NoSuchContentException;
 import io.openvidu.java.client.Connection;
 import io.openvidu.java.client.ConnectionProperties;
 import io.openvidu.java.client.OpenVidu;
+import io.openvidu.java.client.OpenViduException;
 import io.openvidu.java.client.Session;
 import io.openvidu.java.client.SessionProperties;
 import lombok.SneakyThrows;
@@ -25,19 +26,25 @@ public class OpenViduWebRtcService implements WebRtcService {
 		this.openVidu = new OpenVidu(url, secret);
 	}
 
-	@SneakyThrows
 	public String initializeSession(SessionProperties sessionProperties) {
-		Session session = openVidu.createSession(sessionProperties);
-		return session.getSessionId();
+		try {
+			Session session = openVidu.createSession(sessionProperties);
+			return session.getSessionId();
+		} catch (OpenViduException ex) {
+			throw new RuntimeException(ex);
+		}
 	}
 
-	@SneakyThrows
 	public String createConnection(String sessionId, ConnectionProperties connectionProperties) {
-		Session session = openVidu.getActiveSession(sessionId);
-		if (session == null)
-			throw new NoSuchContentException("해당 세션을 찾을 수 없습니다.");
+		try {
+			Session session = openVidu.getActiveSession(sessionId);
+			if (session == null)
+				throw new NoSuchContentException("해당 세션을 찾을 수 없습니다.");
 
-		Connection connection = session.getConnection(sessionId);
-		return connection.getToken();
+			Connection connection = session.getConnection(sessionId);
+			return connection.getToken();
+		} catch (OpenViduException ex) {
+			throw new RuntimeException(ex);
+		}
 	}
 }
