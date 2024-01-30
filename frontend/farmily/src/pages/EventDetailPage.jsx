ff5e39b818cc18518350769a3f1166e24a98dbb4
지민 <81950388+jjm6604@users.noreply.github.com>
2024-01-30 14:02:01 +0900

    feat: 이벤트 상세 페이지

--- frontend/farmily/src/pages/EventDetailPage.jsx
+++ frontend/farmily/src/pages/EventDetailPage.jsx
@@ -1,39 +1,46 @@
-import chunsik from '../assets/images/chunsik.jpg';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import EventCard from '../components/common/EventCard.jsx';
-
-const TESTITEM = {
-  id: 13,
-  title: '게시글 제목',
-  family: '춘식라이언 가족',
-  author: { nickname: '작성자' },
-  picture: [
-    { image: chunsik, content: '사진1 내용1' },
-    { image: chunsik, content: '사진2 내용2' },
-    { image: chunsik, content: '사진3 내용2' },
-    { image: chunsik, content: '사진4 내용2' },
-    { image: chunsik, content: '사진5 내용2' },
-    { image: chunsik, content: '사진6 내용2' },
-    { image: chunsik, content: '사진7 내용2' },
-    { image: chunsik, content: '사진8 내용2' },
-    { image: chunsik, content: '사진9 내용2' },
-    { image: chunsik, content: '사진10 내용2' },
-    { image: chunsik, content: '사진11 내용2' },
-  ],
-};
+import axios from '../api/axios.jsx';
 
 export default function EventDetailPage() {
+  const { recordId } = useParams();
+  const URL = `/record/${recordId}`;
+
+  const [record, setRecord] = useState({
+    title: '',
+    content: '',
+    author: { nickname: '' },
+    createdAt: '',
+    imageCards: [
+      { image: { location: '', originalFileName: '' }, description: '' },
+    ],
+    comments: [],
+  });
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((response) => {
+        setRecord(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="h-full w-full overflow-hidden">
       <div className="h-1/6 p-10">
         <div className="border-4 p-2 border-black rounded-xl text-left">
-          <p>{TESTITEM.title}</p>
+          <p>{record.title}</p>
         </div>
       </div>
-      <p className="text-right px-10">작성자 : {TESTITEM.author.nickname}</p>
-      <div className="flex h-3/5 snap-x overflow-x-scroll px-6">
-        <div className="h-full flex m-auto">
-          {TESTITEM.picture.map((item, index) => (
-            <EventCard key={index} image={item.image} content={item.content} />
+      <p className="text-right px-10">작성자 : {record.author.nickname}</p>
+      <div className="flex h-3/5 snap-x overflow-x-scroll px-6 justify-center">
+        <div className="h-full w-full flex m-auto">
+          {record.imageCards.map((item, index) => (
+            <EventCard key={index} {...item} />
           ))}
         </div>
       </div>
