3257d7e93c86e2da968e013eb606975d5e08ccda
구본웅 <thesmartist3@gmail.com>
2024-01-31 17:44:40 +0900

    fix: FamilyMembership role @Enumerated EnumType.STRING

--- backend/Farmily/src/main/java/com/ssafy/farmily/entity/FamilyMembership.java
+++ backend/Farmily/src/main/java/com/ssafy/farmily/entity/FamilyMembership.java
@@ -4,6 +4,7 @@ import com.ssafy.farmily.type.FamilyRole;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -36,6 +37,6 @@ public class FamilyMembership extends BaseEntity {
 	private Family family;
 
 	@Column(columnDefinition = "VARCHAR(32)")
-	@Enumerated
+	@Enumerated(EnumType.STRING)
 	private FamilyRole role;
 }
