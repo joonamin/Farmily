56961309864101f4349e29693fe09f667a1d34b1
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-01-31 11:19:02 +0900

    feat: accessToken Redux 구현

--- frontend/farmily/src/api/axios.jsx
+++ frontend/farmily/src/api/axios.jsx
@@ -1,15 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
 // 토큰 값 설정
-const token ='eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJHT09HTEVfMTA2MTA2ODE1MiIsImV4cCI6MTcwNjU3NzU5NSwiaWF0IjoxNzA2NTc2Njk1LCJpc3MiOiJmYXJtaWx5Iiwicm9sZXMiOiJVU0VSIn0.9qTAwDfkoy7W11eWxuk7m2q6e95pmmRBb-vCJ2pCrceM2inTcxyQCYnELiRhLB7r7gZZvKR5Xt-lKDLVbGPj8A'
- 
+const token = '';
+
 const accessToken = `Bearer ${token}`;
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    Authorization: accessToken,
-  },
+  // headers: {
+  //   Authorization: accessToken,
+  // },
 });
 
 export default instance;
--- frontend/farmily/src/pages/MainPage.jsx
+++ frontend/farmily/src/pages/MainPage.jsx
@@ -4,12 +4,26 @@ import MainTree from '../components/tree/MainTree.jsx';
 import Challenge from '../components/tree/Challenge.jsx';
 import Board from '../components/tree/Board.jsx';
 
+import { useDispatch } from 'react-redux';
+import { getAccessToken } from '../store/auth.jsx';
+
 export default function MainPage() {
+  const dispatch = useDispatch();
+
+  const tokenString = document.cookie.match('accessToken').input;
+
+  const accessToken = tokenString.split('accessToken=')[1];
+  // console.log(accessToken);
+
+  useEffect(() => {
+    dispatch(getAccessToken({ accessToken: accessToken }));
+  }, []);
+
   return (
     <>
       <Challenge />
       <MainTree />
-      <Board />
+      <Board title="나무 꾸미기" />
     </>
   );
 }
