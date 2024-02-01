3b6180aa54719578cd97a1bc48dcb2cf1d02f81d
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-02-01 10:40:13 +0900

    feat: 추억보기 디테일 페이지 글쓰기 버튼 안보이게

--- frontend/farmily/src/pages/RecordPage.jsx
+++ frontend/farmily/src/pages/RecordPage.jsx
@@ -1,20 +1,36 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import ArticleList from '../components/common/ArticleList.jsx';
 import ImageList from '../components/common/ImageList.jsx';
 import SmallButton from '../components/button/SmallButton.jsx';
 
 export default function RecordPage() {
+  const family = useSelector((state) => state.family.value);
   const { sprintId } = useParams();
+
+  // 현재 sprintId 와 페이지 sprintId가 같을때만 글쓰기 버튼 존재
+  const [isValid, setIsValid] = useState(false);
+
+  useEffect(() => {
+    if (family.sprintId === parseInt(sprintId)) {
+      setIsValid(true);
+    }
+  }, [family.sprintId, sprintId]);
+
+  console.log(isValid);
+
   return (
     <>
       <h1>통계 자리</h1>
       <ImageList />
       <ArticleList sprintId={sprintId} />
       {/* 현재 진행중 sprintId와 같은 경우에만 보이게 수정하기 */}
-      <div className="flex justify-end top-0 right-0">
-        <SmallButton text="글쓰기" url="/family/record/create" />
-      </div>
+      {isValid && (
+        <div className="flex justify-end top-0 right-0">
+          <SmallButton text="글쓰기" url="/family/record/create" />
+        </div>
+      )}
     </>
   );
 }
