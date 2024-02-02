2b074f40218ad27b3060c480dad58ad95d2ea1a6
지민 <81950388+jjm6604@users.noreply.github.com>
2024-02-02 14:09:40 +0900

    feat: 댓글 작성/조회

--- frontend/farmily/src/components/common/Comment.jsx
+++ frontend/farmily/src/components/common/Comment.jsx
@@ -3,11 +3,15 @@ import { useState, useEffect } from 'react';
 import CommentList from './CommentList.jsx';
 import CommentCreate from './CommentCreate.jsx';
 
-export default function Comment() {
+export default function Comment({ comments, recordId, onCommentCreate }) {
   return (
     <div className="w-full h-4/6 mx-auto">
-      <CommentCreate />
-      <CommentList />
+      <CommentCreate
+        recordId={recordId}
+        comments={comments}
+        onCommentCreate={onCommentCreate}
+      />
+      <CommentList comments={comments} />
     </div>
   );
 }
--- frontend/farmily/src/components/common/CommentCreate.jsx
+++ frontend/farmily/src/components/common/CommentCreate.jsx
@@ -1,21 +1,42 @@
 import { useState, useEffect } from 'react';
+import axios from '../../api/axios.jsx';
 
-export default function CommentCreate() {
+export default function CommentCreate({ recordId, onCommentCreate }) {
+  const [content, setContent] = useState();
+  const handleClick = () => {
+    axios
+      .post(`record/${recordId}/comment`, {
+        content: content,
+      })
+      .then((response) => {
+        onCommentCreate();
+        setContent('');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleChange = (e) => {
+    setContent(e.target.value);
+  };
+
   return (
     <div>
-      <form className="flex items-center rounded-lg pb-4">
+      <div className="flex items-center rounded-lg pb-4">
         <textarea
           type="text"
           placeholder="댓글을 입력하세요."
           className="border-4 flex-grow py-1 resize-none border-black p-2 rounded-xl"
+          value={content}
+          onChange={handleChange}
         ></textarea>
         <button
-          type="submit"
           className="bg-stone-300 text-black ml-2 whitespace-pre-line rounded-lg p-5"
+          onClick={handleClick}
         >
           작성
         </button>
-      </form>
+      </div>
     </div>
   );
 }
