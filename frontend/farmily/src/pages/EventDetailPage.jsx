1409549deeae04c0e4ccfb2e2c517bc194a0a0ad
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-02-02 15:58:02 +0900

    feat: 캘린더 일정 추가

--- frontend/farmily/src/pages/EventDetailPage.jsx
+++ frontend/farmily/src/pages/EventDetailPage.jsx
@@ -31,13 +31,18 @@ export default function EventDetailPage() {
   }, []);
 
   return (
-    <div className="h-full w-full overflow-hidden">
-      <div className="h-1/6 p-10">
+    <div className="h-full w-full overflow-hidden p-10">
+      <div className="h-1/12">
         <div className="border-4 p-2 border-black rounded-xl text-left">
           <p>{record.title}</p>
         </div>
       </div>
-      <p className="text-right px-10">작성자 : {record.author.nickname}</p>
+      <div className="flex justify-between">
+        <span className="text-left">
+          작성일자 : {record.createdAt.slice(0, 10)}
+        </span>
+        <span className="text-right">작성자 : {record.author.nickname}</span>
+      </div>
       <div className="flex h-3/5 snap-x overflow-x-scroll px-6 justify-center">
         <div className="h-full w-full flex m-auto">
           {record.imageCards.map((item, index) => (
