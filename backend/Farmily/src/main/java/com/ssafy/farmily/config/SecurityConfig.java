903e4febc043a2f8f7d7dba2c73611aec951038f
SeoJunHa96 <139415975+SeoJunHa96@users.noreply.github.com>
2024-02-07 08:59:34 +0900

    feat : 뽑기 기능 구현

--- backend/Farmily/src/main/java/com/ssafy/farmily/config/SecurityConfig.java
+++ backend/Farmily/src/main/java/com/ssafy/farmily/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
-			.csrf(Customizer.withDefaults())
+			.csrf(AbstractHttpConfigurer::disable) 
 			.sessionManagement(sm -> {
 				sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 				sm.sessionFixation(SessionManagementConfigurer.SessionFixationConfigurer::none);
--- frontend/farmily/src/components/tree/BoardModal.jsx
+++ frontend/farmily/src/components/tree/BoardModal.jsx
@@ -1,11 +1,33 @@
-  import React, { useState } from "react";
+  import React, { useState, useEffect } from "react";
   import SmallButton from "../button/SmallButton";
   import Draw from "./Draw";
   import Collection from "./Collection";
   import Fruits from "./Fruits";
+  import { useParams } from 'react-router-dom';
+  import axios from '../../api/axios.jsx'
 
   const BoardModal = ({ isOpen, closeModal }) => {
     const [selectedTab, setSelectedTab] = useState("segment1");
+    const { familyId } = useParams();
+    const [isRaffling, setIsRaffling] = useState(false);
+    const [rafflingResponse, setRafflingResponse] = useState(null);
+    const [familyPoint, setFamilyPoint] = useState(0);
+    
+    useEffect(() => {
+      // familyPoint
+      const fetchFamilyPoint = async () => {
+        try {
+          const response = await axios.get(`/family/point/${familyId}`);
+          setFamilyPoint(response.data.point);
+        } catch (error) {
+          console.error('Fetching family point error:', error);
+        }
+      };
+  
+      if (isOpen) {
+        fetchFamilyPoint();
+      }
+    }, [isOpen, familyId]);
 
     const handleOutsideClick = (e) => {
       if (e.target.id === "my-modal") {
@@ -13,19 +35,59 @@
       }
     };
 
+    
     const handleTabClick = (tab) => {
       setSelectedTab(tab);
     };
 
+    //  뽑기 로직
+    const handleRaffle = async () => {
+      if (familyPoint < 100) {
+        alert("포인트가 부족합니다.");
+        return;
+      }
+      setIsRaffling(true); // 뽑기 시작
+  
+      try {
+        const response = await axios.post('/family/raffling', { familyId });
+        setRafflingResponse(response.data);
+        setFamilyPoint(prev => prev - 100); // 포인트 사용
+      } catch (error) {
+        console.error('Raffle error:', error);
+      } finally {
+        setIsRaffling(false); // 뽑기 종료
+      }
+    };
+
+    useEffect(() => {
+      // Event listener for clicking outside the modal
+      const handleDocumentClick = (e) => {
+        if (!isOpen || e.target.closest("#my-modal")) {
+          return; // Don't close if modal is not open or if click is inside modal
+        }
+        closeModal();
+      };
+  
+      // Add event listener when the modal is open
+      if (isOpen) {
+        document.addEventListener("click", handleDocumentClick);
+      }
+  
+      // Remove event listener when the modal is closed or unmounted
+      return () => {
+        document.removeEventListener("click", handleDocumentClick);
+      };
+    }, [isOpen, closeModal]);
+  
     return (
       <>
         {isOpen && (
           <div
-            className="fixed inset-0 flex items-center justify-center z-50 ml-[12%] px-[20%] mb-40"
+            className="fixed inset-0 flex items-center justify-center z-50 ml-[5%] px-[20%] mb-40 py-40 pb-10"
             id="my-modal"
             onClick={handleOutsideClick}
           >
-            <div className="relative p-5 mx-auto bg-white border rounded-md shadow-lg top-20 w-96 h-96 flex flex-col">
+            <div className="relative p-5 mx-auto bg-white border rounded-md shadow-lg w-96 h-full flex flex-col">
               <div className="bg-gray-200 rounded-lg mb-5" style={{ padding: "2px" }}>
                 <div className="relative flex items-center">
                   {/* Tab dividers */}
@@ -84,19 +146,25 @@
               {selectedTab === "segment2" && (
                  <div className="overflow-auto max-h-[calc(100vh-10rem)]">
                   <Collection />
-                </div>
+                </div>  
               )}
               {selectedTab === "segment3" && (
                 <div className="overflow-auto max-h-[calc(100vh-10rem)]">
-                  <Draw />
+                  <Draw
+                    isRaffling={isRaffling}
+                    rafflingResponse={rafflingResponse}
+                    familyPoint={familyPoint} // Draw 컴포넌트에 familyPoint 전달
+                  />
                 </div>
               )}
-             <div className="mt-auto">
-
-              <div className="flex justify-end">
-                <span onClick={closeModal}><SmallButton text="닫기" /></span>
-              </div>
-             </div>
+        <div className="mt-auto w-full">
+          <div className="flex justify-end items-center">
+            {selectedTab === "segment3" && (
+              <span className="mr-auto" onClick={handleRaffle}><SmallButton text="상자 열기" /></span>
+            )}
+            <span onClick={closeModal}><SmallButton text="닫기" /></span>
+            </div>
+          </div>
             </div>
           </div>
         )}
