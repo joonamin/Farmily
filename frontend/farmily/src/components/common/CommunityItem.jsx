c53f23c6d0ad7466452fd33ddaca91983f7e38e0
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-02-06 14:56:12 +0900

    feat: 커뮤니티 디테일

--- frontend/farmily/src/components/common/CommunityItem.jsx
+++ frontend/farmily/src/components/common/CommunityItem.jsx
@@ -6,7 +6,7 @@ export default function CommunityItem(community) {
   return (
     // 이미지, 가족명, 가족아이디, 자랑하기 내용 받아오기
     <div className="p-5 snap-center">
-      <Link to={`/family/community/${community.id}`}>
+      <Link to={`${community.id}`}>
         <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700 p-3">
           <img className="rounded-lg" src={communityTree} alt="tree" />
           <h3 className="text-lg mt-2">{community.title}</h3>
