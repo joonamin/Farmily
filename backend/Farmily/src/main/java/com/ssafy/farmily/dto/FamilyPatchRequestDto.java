bf2ea0cf76b7900954f59a80175d277eb98af547
구본웅 <thesmartist3@gmail.com>
2024-02-07 09:05:10 +0900

    feat: FamilyPatchRequestDto.Image 추가

--- backend/Farmily/src/main/java/com/ssafy/farmily/dto/FamilyPatchRequestDto.java
+++ backend/Farmily/src/main/java/com/ssafy/farmily/dto/FamilyPatchRequestDto.java
@@ -1,5 +1,10 @@
 package com.ssafy.farmily.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ssafy.farmily.type.FileCategory;
+import com.ssafy.farmily.validation.annotation.AllowedFileCategories;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,5 +33,16 @@ public class FamilyPatchRequestDto {
 	public static class Motto {
 		@NotNull
 		private String newMotto;
+	}
+
+	@Getter
+	@Setter
+	@RequiredArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	public static class Image {
+		@NotNull
+		@AllowedFileCategories(categories = FileCategory.IMAGE)
+		private MultipartFile multipartFile;
 	}
 }
