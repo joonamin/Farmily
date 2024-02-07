ec0b7eddbac311165e87daebeb6190817f5c90c3
joonamin <joonamin44@gmail.com>
2024-02-07 16:22:07 +0900

    deploy: 프론트엔드, 백엔드 배포 및 수동 배포 관련 스크립트 작성

--- frontend/farmily/src/components/button/LoginButton.jsx
+++ frontend/farmily/src/components/button/LoginButton.jsx
@@ -1,12 +1,13 @@
 export default function LoginButton(site) {
-  return (
-    <button className="h-2/3">
-      <a
-        href={`http://localhost:8080/oauth2/authorization/${site.url}`}
-      >
-        <img className="h-2/3 object-cover" src={site.image} alt="logo" />
-        <p>{site.name} 로그인</p>
-      </a>
-    </button>
-  );
+    const baseUrl = import.meta.env.VITE_API_URL
+    return (
+        <button className="h-2/3">
+            <a
+                href={`${baseUrl}/oauth2/authorization/${site.url}`}
+            >
+                <img className="h-2/3 object-cover" src={site.image} alt="logo"/>
+                <p>{site.name} 로그인</p>
+            </a>
+        </button>
+    );
 }
