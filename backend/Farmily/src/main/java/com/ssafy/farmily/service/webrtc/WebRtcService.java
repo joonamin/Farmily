4174da209fb6b699f950fc4174ebf6b530977fc3
구본웅 <thesmartist3@gmail.com>
2024-02-13 13:38:32 +0900

    feat: webhook service skeleton

--- backend/Farmily/src/main/java/com/ssafy/farmily/service/webrtc/WebRtcService.java
+++ backend/Farmily/src/main/java/com/ssafy/farmily/service/webrtc/WebRtcService.java
@@ -1,5 +1,6 @@
 package com.ssafy.farmily.service.webrtc;
 
+import com.ssafy.farmily.dto.OpenViduWebhookEventDto;
 import com.ssafy.farmily.entity.Conference;
 import com.ssafy.farmily.dto.ConferenceJoinResponseDto;
 
@@ -9,4 +10,12 @@ public interface WebRtcService {
 	Conference createConference(Long familyId);
 
 	String joinConference(String username, Conference conference);
+
+	void onSessionCreated(OpenViduWebhookEventDto.SessionCreated dto);
+
+	void onSessionDestroyed(OpenViduWebhookEventDto.SessionDestroyed dto);
+
+	void onParticipantJoined(OpenViduWebhookEventDto.ParticipantJoined dto);
+
+	void onParticipantLeft(OpenViduWebhookEventDto.ParticipantLeft dto);
 }
