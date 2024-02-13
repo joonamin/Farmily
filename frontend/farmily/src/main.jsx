2c57f71163f24900a1a1086ded306b81538a82f6
지민 <81950388+jjm6604@users.noreply.github.com>
2024-02-13 17:39:45 +0900

    fix: 새로고침 오류 수정

--- frontend/farmily/src/main.jsx
+++ frontend/farmily/src/main.jsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-import store from './store/store.jsx';
+import { store, persistor } from './store/store.jsx';
 import { Provider } from 'react-redux';
 
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
-
-export let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
