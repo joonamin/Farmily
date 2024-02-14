1507f0c0933b1e7fa182f0f7d01bdd95d2fbed26
지민 <81950388+jjm6604@users.noreply.github.com>
2024-02-14 10:32:36 +0900

    feat: 사이드바 active

--- frontend/farmily/src/components/SideBar.jsx
+++ frontend/farmily/src/components/SideBar.jsx
@@ -15,28 +15,22 @@ export default function SideBar() {
   const family = useSelector((state) => state.family.value);
   const [familyName, setFamilyName] = useState('가족');
   const [category, setCategory] = useState([
-    {
-      name: '📑 기록하기',
-      url: `/family/record/${family ? family.mainSprint.sprintId : 0}`,
-    },
-    { name: '🌳 추억보기', url: '/family/memory' },
-    { name: '📅 일정보기', url: '/family/calendar' },
-    { name: '🖼 커뮤니티', url: '/family/community' },
-    { name: '🏆 업적보기', url: '/family/achievement' },
-    { name: '💬 소통하기', url: '/family/contact' },
+    { name: '📑 기록하기', url: `/family/record/${family ? family.mainSprint.sprintId : 0}`, category: 'record' },
+    { name: '🌳 추억보기', url: '/family/memory', category: 'memory' },
+    { name: '📅 일정보기', url: '/family/calendar', category: 'calendar' },
+    { name: '🖼 커뮤니티', url: '/family/community', category: 'community' },
+    { name: '🏆 업적보기', url: '/family/achievement', category: 'achievement' },
+    { name: '💬 소통하기', url: '/family/contact', category: 'contact' },
   ]);
   useEffect(() => {
     setFamilyName(family.name);
     setCategory([
-      {
-        name: '📑 기록하기',
-        url: `/family/record/${family.mainSprint.sprintId}`,
-      },
-      { name: '🌳 추억보기', url: '/family/memory' },
-      { name: '📅 일정보기', url: '/family/calendar' },
-      { name: '🖼 커뮤니티', url: '/family/community' },
-      { name: '🏆 업적보기', url: '/family/achievement' },
-      { name: '💬 소통하기', url: '/family/contact' },
+      { name: '📑 기록하기', url: `/family/record/${family.mainSprint.sprintId}`, category: 'record' },
+      { name: '🌳 추억보기', url: '/family/memory', category: 'memory' },
+      { name: '📅 일정보기', url: '/family/calendar', category: 'calendar' },
+      { name: '🖼 커뮤니티', url: '/family/community', category: 'community' },
+      { name: '🏆 업적보기', url: '/family/achievement', category: 'achievement' },
+      { name: '💬 소통하기', url: '/family/contact', category: 'contact' },
     ]);
   }, [family]);
 
@@ -71,6 +65,7 @@ export default function SideBar() {
           {category.map((categoryItem, categoryIndex) => (
             <SideButton
               key={categoryIndex}
+              category={categoryItem.category}
               name={categoryItem.name}
               url={categoryItem.url}
             />
